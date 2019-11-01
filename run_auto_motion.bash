7d48c396ea5ad70427cfb2949bf7748a38bf6821
Dani Cosme <dani.cosme@gmail.com>
2019-11-01 14:24:42 -0700

    Updated sbatch parameters

--- run_auto_motion.bash
+++ run_auto_motion.bash
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=auto-motion-fmriprep
 #SBATCH --output=auto-motion-fmriprep.log
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1000
+#SBATCH --mem-per-cpu=1G
 #SBATCH --partition=short,fat,long,longfat
 
 module load R gcc
 
-srun Rscript --verbose auto_motion_fmriprep.R
+Rscript --verbose auto_motion_fmriprep.R
