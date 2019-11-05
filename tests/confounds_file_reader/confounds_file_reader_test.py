edb051f1cfc19dfb926cc1699893019b21c270cf
Paul Novak <pnovak@alumni.caltech.edu>
2019-11-05 10:48:54 -0800

    Add unit tests for old fmriprep confounds format

--- tests/confounds_file_reader/confounds_file_reader_test.py
+++ tests/confounds_file_reader/confounds_file_reader_test.py
@@ -26,10 +26,12 @@ class TestConfoundsFileReader:
         task = 'WTP'
         run = '4'
         file_name = f'sub-{subject_id}_ses-wave{wave}_task-{task}_acq-{run}_desc-confounds_regressors.tsv'
-        data_file = tmp_path.joinpath(file_name)
+        confounds_path = tmp_path / "confounds"
+        confounds_path.mkdir()
+        data_file = confounds_path / file_name
         shutil.copyfile((shared_datadir / 'test_confounds.tsv'), data_file)
 
-        c = ConfoundsFileReader(tmp_path)
+        c = ConfoundsFileReader(confounds_path)
 
         # Assert that data has a subject ID, wave number, task name and run number
         for i, _ in c.get_confounds():
@@ -46,19 +48,22 @@ class TestConfoundsFileReader:
         wave = "1"
         task = 'WTP'
 
+        confounds_path = tmp_path / "confounds"
+        confounds_path.mkdir()
+
         num_files = 4
         for run in range(num_files):
             file_name = f'sub-{subject_id}_ses-wave{wave}_task-{task}_acq-{run}_desc-confounds_regressors.tsv'
-            data_file = tmp_path.joinpath(file_name)
+            data_file = confounds_path / file_name
             shutil.copyfile((shared_datadir / 'test_confounds.tsv'), data_file)
 
-        c = ConfoundsFileReader(tmp_path)
+        c = ConfoundsFileReader(confounds_path)
 
         # Assert that several files were read, and that the data has the "right" shape
         count = 0
         for _, data in c.get_confounds():
             assert data.shape == (2,)
-            assert len(data[0]) == len(c._names)
+            assert len(data[0]) == len(c._input_names)
             count += 1
 
         assert count == num_files
@@ -74,4 +79,29 @@ class TestConfoundsFileReader:
         # There are two lines of input data.
         # Length of data should be length of _names + 1, for the additional 'artifact' column in training data.
         assert data.shape == (2,)
-        assert len(data[0]) == len(c._names) + 1
+        assert len(data[0]) == len(c._input_names) + 1
+
+    def test_v1_1_file_format(self, tmp_path, shared_datadir):
+        # Verify that ConfoundsFileReader can read data in the V1.1 fmriprep format.
+        # The primary differences are that the files are named differently:
+        # "sub-<sub_id>/func/sub-<sub_id>_task-<task_id>_run-<run_id>_confounds.tsv",
+        # that the column names are represented differently: WhiteMatter compared with white_matter,
+        # and that there are many many fewer confounds in the older format.
+        subject_id = 'DEV001'
+        wave = '1'
+        task = 'WTP'
+        run = '1'
+
+        file_name = f'sub-{subject_id}_ses-wave{wave}_task-{task}_run-{run}_confounds.tsv'
+        confounds_path = tmp_path / "confounds"
+        confounds_path.mkdir()
+        data_file = confounds_path / file_name
+        shutil.copyfile((shared_datadir / 'test_v1_1_confounds.tsv'), data_file)
+
+        c = ConfoundsFileReader(confounds_path, is_v1_1_format=True)
+
+        # Assert that several files were read, and that the data has the "right" shape
+        for _, data in c.get_confounds():
+            assert data.shape == (2,)
+            assert len(data[0]) == len(c._input_names)
+            assert 'csf' in data.dtype.names
